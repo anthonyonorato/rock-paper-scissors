89db21adea4fa5c5e37fc5fa86c9396adac1deb4
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-07-15 16:04:38 -0400

    troubleshooting rock symbol

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function updateScoreboard(roundResult){
   let computerSign = document.getElementById('cpu' + round.toString())
   switch (playerSelection){
     case 'ROCK':
-      playerSign.innerText = '💎'
+      playerSign.innerText = '💎💎'
       break
     case 'PAPER':
       playerSign.innerText = '🧻'
@@ -71,7 +71,7 @@ function updateScoreboard(roundResult){
   }
   switch (computerSelection){
     case 'ROCK':
-      computerSign.innerText = '💎'
+      computerSign.innerText = '💎💎'
       break
     case 'PAPER':
       computerSign.innerText = '🧻'
